e7cd5c904b4ef4199948648092ac490db0d3aa92
Anderson Nunez <noandersonnls@gmail.com>
2023-06-08 23:00:14 -0400

    build: nextjs 13 starter build

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from './context/theme'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Logicea Test',
+  description: 'Test with Jokes API from Retool',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
