1829d9e640143bc3af99c699a958651e4f5a8601
Anderson Nunez <noandersonnls@gmail.com>
2023-06-09 18:05:31 -0400

    feat: login and logout feature

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -8,7 +8,6 @@ function Login() {
   const router = useRouter();
   useEffect(() => {
     const token = window.localStorage.getItem('token');
-    console.log(token);
     if (token) {
       router.push('/jokes');
     }
